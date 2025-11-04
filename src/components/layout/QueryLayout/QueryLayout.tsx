8aaa253b8b534ff930d8e7f80135f39a536f9bba
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-04 22:20:23 +0100

    feat: centralised layout for fetched contents

---
+++ src/components/layout/QueryLayout/QueryLayout.tsx
@@ -0,0 +1,20 @@
+import { useApiClient } from "@/core/hooks/api-client";
+import type { QueryLayoutProps } from "./model";
+
+export default function QueryLayout<T>(props: QueryLayoutProps<T>) {
+    const { data, isLoading, isError, error } = useApiClient<T>(
+        props.queryClientOptions,
+    );
+
+    if (isLoading) {
+        return <>...loading</>;
+    }
+
+    if (isError) {
+        return <>{error?.message}</>;
+    }
+
+    if (data) {
+        return <>{props.children(data)}</>;
+    }
+}
