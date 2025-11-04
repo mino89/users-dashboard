7f60fe3ce5ee4e8601c3c28737575d62311e0444
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-04 23:04:14 +0100

    fix: changelog prepend path

--- changelog.sh
+++ changelog.sh
@@ -5,5 +5,5 @@ NODE_VERSION=$(node -p -e "require('./package.json').version")
 if [ "$1" = "stdout" ]; then
     pnpm exec git-cliff -o - --unreleased --tag $NODE_VERSION
 else
-    pnpm exec git-cliff -o './CHANGELOG.md' --prepend --tag $NODE_VERSION
+    pnpm exec git-cliff -o './CHANGELOG.md' --prepend './CHANGELOG.md' --tag $NODE_VERSION
 fi
