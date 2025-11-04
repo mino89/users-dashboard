b2404d49eca00c0cd54f0898810be4d8b9ee268e
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-04 22:21:19 +0100

    feat: layout with data for list and detail

--- src/features/Detail.tsx
+++ src/features/Detail.tsx
@@ -1,3 +1,6 @@
+import QueryLayout from "@/components/layout/QueryLayout/QueryLayout";
+import Json from "@/components/utils/Json/Json";
+import type { User } from "@/types/user";
 import { getRouteApi } from "@tanstack/react-router";
 
 const routeApi = getRouteApi("/$id");
@@ -7,6 +10,14 @@ export function Detail() {
     return (
         <div className="p-2">
             <h1>Detail Page {id}</h1>
+            <QueryLayout<User>
+                queryClientOptions={{
+                    queryKeys: ["user"],
+                    path: [id],
+                }}
+            >
+                {(data) => <Json obj={data} />}
+            </QueryLayout>
         </div>
     );
 }
--- src/features/Index.tsx
+++ src/features/Index.tsx
@@ -1,3 +1,18 @@
+import QueryLayout from "@/components/layout/QueryLayout/QueryLayout";
+import Json from "@/components/utils/Json/Json";
+import type { UsersList } from "@/types/user";
+
 export function Index() {
-    return <h1>Welcome Home!</h1>;
+    return (
+        <>
+            <h1>Welcome Home!</h1>
+            <QueryLayout<UsersList>
+                queryClientOptions={{
+                    queryKeys: ["user"],
+                }}
+            >
+                {(data) => <Json obj={data.users} />}
+            </QueryLayout>
+        </>
+    );
 }
