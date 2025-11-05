a70dc884468c0a4f9764cf21337e8b47d05fd660
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-05 14:54:12 +0100

    feat: input text creation

---
+++ src/components/shared/Input/InputText.tsx
@@ -0,0 +1,14 @@
+import type { InputTextProps } from "../types/input";
+
+export default function InputText(props: InputTextProps) {
+    const { value, placeholder, onChange, ariaLabel, type } = props;
+    return (
+        <input
+            aria-label={ariaLabel}
+            placeholder={placeholder}
+            value={value}
+            type={type || "text"}
+            onChange={(e) => onChange?.(e.target.value)}
+        />
+    );
+}
