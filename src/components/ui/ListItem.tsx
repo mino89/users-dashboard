f0bf80bdb35d166a4b3f0d0938c76bdc033b8a82
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-06 16:41:49 +0100

    feat: list item implementation

---
+++ src/components/ui/ListItem.tsx
@@ -0,0 +1,20 @@
+import { Link } from "@tanstack/react-router";
+import classes from "./ListItem.module.css";
+type ListItemProps = {
+    title: string;
+    subtitle?: string;
+    children?: React.ReactNode;
+    link?: string;
+};
+
+export default function ListItem(props: ListItemProps) {
+    const { title, subtitle, children, link } = props;
+    return (
+        <article className={classes.listItem}>
+            <h3>{title}</h3>
+            {subtitle && <h4>{subtitle}</h4>}
+            {children}
+            {link && <Link to={link}>More Info</Link>}
+        </article>
+    );
+}
