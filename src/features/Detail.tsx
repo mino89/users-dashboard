5fded61907e5391c8d4f9770c10767af59ed2ccd
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-07 01:57:57 +0100

    feat: detail page structure implementation

--- src/features/Detail.tsx
+++ src/features/Detail.tsx
@@ -1,14 +1,15 @@
 import QueryLayout from "@components/layout/QueryLayout";
-import Json from "@components/utils/Json";
+import List from "@components/ui/List";
+import ListItem from "@components/ui/ListItem";
+import SectionHeading from "@components/ui/SectionHeading";
 import { getRouteApi } from "@tanstack/react-router";
 import type { User } from "@type/data/user";
 
-const routeApi = getRouteApi("/$id");
-
 export function Detail() {
+    const routeApi = getRouteApi("/$id");
     const id = routeApi.useParams().id;
     return (
-        <div className="p-2">
+        <>
             <h1>Detail Page {id}</h1>
             <QueryLayout<User>
                 queryClientOptions={{
@@ -16,8 +17,46 @@ export function Detail() {
                     path: [id],
                 }}
             >
-                {(data) => <Json obj={data} />}
+                {(data) => (
+                    <>
+                        <SectionHeading
+                            title={data.firstName + " " + data.lastName}
+                            subtitle={data.email}
+                            image={{
+                                url: data.image,
+                                alt: data.firstName + " " + data.lastName,
+                            }}
+                            link={{
+                                url: "../",
+                                text: "Back to Users List",
+                            }}
+                        >
+                            <p>Role: {data.role}</p>
+                        </SectionHeading>
+                        <List columns="large">
+                            <ListItem
+                                title="Personal Info"
+                                subtitle={data.phone}
+                            >
+                                <p>Blood Group: {data.bloodGroup}</p>
+                                <p>
+                                    Address: {data.address.address},{" "}
+                                    {data.address.city}, {data.address.state},{" "}
+                                    {data.address.postalCode},{" "}
+                                    {data.address.country}
+                                </p>
+                            </ListItem>
+                            <ListItem
+                                title="Company Info"
+                                subtitle={data.company.name}
+                            >
+                                <p>Department: {data.company.department}</p>
+                                <p>Title: {data.company.title}</p>
+                            </ListItem>
+                        </List>
+                    </>
+                )}
             </QueryLayout>
-        </div>
+        </>
     );
 }
--- src/types/data/user.ts
+++ src/types/data/user.ts
@@ -1,11 +1,36 @@
 export type UserRole = "admin" | "user" | "moderator";
 
+export type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+export type UserAddress = {
+  address: string;
+  city: string;
+  postalCode: string;
+  state: string;
+  country: string;
+  coordinates: LatLng;
+};
+
+export type UserCompany = {
+  name: string;
+  department: string;
+  title: string;
+};
+
 export type User = {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
   image: string;
+  phone: string;
+  gender: string;
+  bloodGroup: string;
+  address: UserAddress;
+  company: UserCompany;
   role: UserRole;
 };
 
