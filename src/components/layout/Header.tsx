267538ba67d5f3708fcf89156a478eadf5a48bc2
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-07 22:28:26 +0100

    feat: header component implementation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,19 @@
+import { Link } from "@tanstack/react-router";
 import classes from "./Header.module.css";
-export default function Header() {
-    return <header className={classes.header}>Header</header>;
+export type HeaderProps = {
+    logo: React.ReactNode;
+    heading: string;
+    actions?: React.ReactNode;
+};
+export default function Header(props: HeaderProps) {
+    const { logo, heading, actions } = props;
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                {logo}
+                <Link to="/"> {heading} </Link>
+            </div>
+            <div className={classes.actions}>{actions}</div>
+        </header>
+    );
 }
--- src/features/Root.tsx
+++ src/features/Root.tsx
@@ -2,9 +2,17 @@ import Footer from "@components/layout/Footer";
 import Header from "@components/layout/Header";
 import Shell from "@components/layout/Shell";
 import { Outlet } from "@tanstack/react-router";
+
+import LogoComponent from "@assets/react.svg?react";
+
 export function Root() {
     return (
-        <Shell header={<Header />} footer={<Footer />}>
+        <Shell
+            header={
+                <Header logo={<LogoComponent />} heading="Users Dashboard" />
+            }
+            footer={<Footer />}
+        >
             <Outlet />
         </Shell>
     );
