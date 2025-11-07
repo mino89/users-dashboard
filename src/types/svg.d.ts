793a3118c15ac0c46c41b78f4827213ac9e6bb77
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-07 21:59:55 +0100

    chore: added vite-plugin-svgr

---
+++ src/types/svg.d.ts
@@ -0,0 +1,15 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg?react" {
+  import { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+  export default content;
+}
