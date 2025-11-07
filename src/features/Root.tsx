10d26fec5714c1e0a74ad083414e44b898410e62
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-07 18:11:45 +0100

    chore: removed unused dependency

--- src/features/Root.tsx
+++ src/features/Root.tsx
@@ -2,7 +2,6 @@ import Footer from "@components/layout/Footer";
 import Header from "@components/layout/Header";
 import Shell from "@components/layout/Shell";
 import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export function Root() {
     return (
         <Shell header={<Header />} footer={<Footer />}>
