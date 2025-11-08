128556c3351039b9737f4df1dc51ec0f17ddd291
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-08 01:28:09 +0100

    feat: header centered on desktop visualization

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,11 +9,13 @@ export default function Header(props: HeaderProps) {
     const { logo, heading, actions } = props;
     return (
         <header className={classes.header}>
-            <div className={classes.logo}>
-                {logo}
-                <Link to="/"> {heading} </Link>
+            <div className={classes.headerContainer}>
+                <div className={classes.logo}>
+                    {logo}
+                    <Link to="/"> {heading} </Link>
+                </div>
+                <div className={classes.actions}>{actions}</div>
             </div>
-            <div className={classes.actions}>{actions}</div>
         </header>
     );
 }
