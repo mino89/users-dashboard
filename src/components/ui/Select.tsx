08aeebd38ad5ab6f89ba926749807376bf0a576e
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-10 12:13:07 +0100

    style(inputs): replaced class instead of direct selector, background-color to optimise readability

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,9 +1,13 @@
 import type { SelectProps } from "@type/core/input";
-import "./Select.module.css";
+import classes from "./Select.module.css";
 export default function Select(props: SelectProps) {
     const { onChange } = props;
     return (
-        <select {...props} onChange={(e) => onChange?.(e.target.value)}>
+        <select
+            className={classes.inputSelect}
+            {...props}
+            onChange={(e) => onChange?.(e.target.value)}
+        >
             {props.options.map((option) => (
                 <option key={option.value} value={option.value}>
                     {option.label}
