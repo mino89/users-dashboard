70c7843d51b86c36cb4b220b04a54342fd0daef8
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2025-11-10 16:24:36 +0100

    docs: documented useTeme hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,10 @@
 import { useContext } from "react";
 import { ThemeContext } from "@context/ThemeContext";
+
+/**
+ * Custom hook to access the ThemeContext.
+ * @returns (TemeContextType)
+ */
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
